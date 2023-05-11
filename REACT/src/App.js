753a289ca8ab1e88313ccdf37673db1a36f0f285
M Hammad <hammadafzal1111@gmail.com>
2023-05-11 06:44:42 +0500

    Lec_7 React Router Setup + Usage

--- REACT/src/App.js
+++ REACT/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Alert from "./components/Alert";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   const [navMode, setNavMode] = useState("light");
   const [alert, setAlert] = useState(null);
@@ -32,21 +34,31 @@ function App() {
 
   return (
     <>
-      <Navbar
-        title="TextUtils"
-        about="About Us"
-        mode={navMode}
-        toggleMode={toggleMode}
-      />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter your text to analyze"
+      <Router>
+        <Navbar
+          title="TextUtils"
+          about="About Us"
           mode={navMode}
+          toggleMode={toggleMode}
         />
-        {/* <About /> */}
-      </div>
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter your text to analyze"
+                  mode={navMode}
+                />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
